5db4dc22bfe36f783bab00bf5a78a2e0c80b7c6d
git_fullname <git_useremail>
2025-01-20 14:44:25 +0100

    adj flake to track unstable branch for both nixos and home manager

--- flake.nix
+++ flake.nix
@@ -25,13 +25,14 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";   
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    #nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";   # prefer rolling release
-    nixos-wsl.url = "github:nix-community/nixos-wsl/main";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";        # prefer rolling release
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";    
+    
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";                #url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
     };
+    
+    nixos-wsl.url = "github:nix-community/nixos-wsl/main";
   };  
 }
