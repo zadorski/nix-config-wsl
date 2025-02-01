2d210db5b82fd5061af8ee27333f133d868fccb3
Pavel Zadorski <pavel.zadorski@gmail.com>
2025-02-01 23:28:11 +0100

    refactor: single file WSL flake minimal (WIP)

--- flake.nix
+++ flake.nix
@@ -1,42 +1,97 @@
 {
-  outputs = {
-    self,
-    nixpkgs,
-    nixos-wsl,
-    home-manager,
-    ...
-  } @ inputs: {
-    nixosConfigurations = {
-      crodax = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {inherit inputs;};
-        modules = [
-          nixos-wsl.nixosModules.default
-          {
-            wsl.defaultUser = "paz"; # be sure to update username
-            nix.settings.experimental-features = ["nix-command" "flakes"];
-            system.stateVersion = "24.05"; # important: keep the value set upon installation
-            wsl.enable = true;
-          }
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true; # it allows the home-manager setup to activate properly inside a `nixos-rebuild build-vm` (helping to avoid `nix-env` like the plague, per https://discourse.nixos.org/t/need-configuration-feedback-and-tips-and-question-about-managing-shell-scripts/17232/3)
-            home-manager.users.paz = import ./home.nix; # be sure to update username
-            #home-manager.extraSpecialArgs = inputs;            # why?
-          }
-        ];
-      };
-    };
-  };
+  description = "WSL minimal flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # prefer rolling release
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-    home-manager = {
-      url = "github:nix-community/home-manager"; #url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nix-ld-rs.url = "github:nix-community/nix-ld-rs"; # this is forgivable for WSL
+  inputs.nix-ld-rs.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nixoswsl.url = "github:nix-community/NixOS-WSL";
+  inputs.nixoswsl.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.vscode-server.url = "github:nix-community/nixos-vscode-server";
+  inputs.vscode-server.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = {
+    nixpkgs,
+    nixoswsl,
+    home-manager,
+    vscode-server,
+    ...
+  }: let
+    username = "paz";
+    systemname = "crodax";
+  in {
+    nixosConfigurations.${systemname} = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixoswsl.nixosModules.wsl
+        vscode-server.nixosModules.default
+        home-manager.nixosModules.home-manager
+        # homemanager inline
+        (
+          {pkgs, ...}: {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.${username} = {
+              home.username = username;
+              home.homeDirectory = "/home/${username}";
+              home.stateVersion = "24.05";
+              home.packages = [
+                pkgs.alejandra # nix formatter
+                pkgs.nil # nix language server
+              ];
+              home.file = {
+                vscode = {
+                  target = ".vscode-server/server-env-setup";
+                  text = ''
+                    # make sure that basic commands are available
+                    PATH=$PATH:/run/current-system/sw/bin/
+                  '';
+                };
+              };
+              programs.home-manager.enable = true;
+            };
+          }
+        )
+        # wsl inline
+        (
+          {pkgs, ...}: {
+            wsl = {
+              enable = true;
+              defaultUser = username;
+            };
+          }
+        )
+        # nixos inline
+        (
+          {pkgs, ...}: {
+            system = {
+              stateVersion = "24.05";
+            };
+            environment.systemPackages = with pkgs; [
+              btop
+              htop
+              wget
+              jq
+              yq-go
+              dnsutils
+            ];
+            nix = {
+              settings.experimental-features = ["nix-command" "flakes"];
+              gc.automatic = true;
+              gc.options = "--delete-older-than 14d";
+            };
+            networking.hostName = systemname;
+          }
+        )
+        # vscode remoting inline
+        (
+          {pkgs, ...}: {
+            programs.nix-ld.enable = true;
+            services.vscode-server.enable = true;
+          }
+        )
+      ];
     };
-    nixos-wsl.url = "github:nix-community/nixos-wsl/main";
   };
 }
