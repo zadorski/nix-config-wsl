10bd9d207bb677afa26707f6c66f9d83b23c4844
zadorski <678169+zadorski@users.noreply.github.com>
2025-05-05 16:56:59 +0200

    fix: bat theme adj path + dracula theme

--- home/bat.nix
+++ home/bat.nix
@@ -4,14 +4,24 @@
     enable = true;
     config = {
       pager = "less -FR";
-      theme = "Catppuccin-mocha";
+      style = "changes,header";
+      theme = "catppuccin-mocha";
     };
+    # bat cache --build
     themes = {
       # https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme
-      Catppuccin-mocha = {
-        src = catppuccin-bat;
-        file = "Catppuccin-mocha.tmTheme";
-      };
+      catppuccin-mocha = builtins.readFile (catppuccin-bat + "themes/Catppuccin Mocha.tmTheme");
+      # https://github.com/Castrozan/.dotfiles/blob/main/nixos/home/programs/common.nix
+      #dracula = {
+      #  src = pkgs.fetchFromGitHub {
+      #    owner = "dracula";
+      #    repo = "sublime"; # Bat uses sublime syntax for its themes
+      #    rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
+      #    sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+      #  };
+      #  file = "Dracula.tmTheme";
+      #};
     };
+    extraPackages = with pkgs.bat-extras; [ batman ];
   };
 }
